51a1c1da6938e9efb5b7231bb4ea7dd4e5b62a22
Sir-Dommy <dominickyengo2017@gmail.com>
2024-12-03 21:19:41 +0300

    Scheme controller logic

--- routes/routeCollection/adminRoutes.php
+++ routes/routeCollection/adminRoutes.php
@@ -21,10 +21,12 @@ Route::group(['prefix'=>'departments'], function(){
 Route::group(['prefix'=>'schemes'], function(){
 
     Route::post('create', [SchemesController::class, 'createScheme']);
-    // Route::put('update', [DepartmentController::class, 'updateDepartment']);
-    // Route::get('get', [DepartmentController::class, 'getSingleDepartment']);
-    // Route::get('', [DepartmentController::class, 'getAllDepartments']);
-    // Route::put('approve/{id}', [DepartmentController::class, 'approveDepartment']);
-    // Route::put('disable/{id}', [DepartmentController::class, 'disableDepartment']);
+    Route::put('update', [SchemesController::class, 'updateScheme']);
+    Route::get('get', [SchemesController::class, 'getSingleScheme']);
+    Route::get('', [SchemesController::class, 'getAllSchemes']);
+    Route::put('approve/{id}', [SchemesController::class, 'approveScheme']);
+    Route::put('disable/{id}', [SchemesController::class, 'disableScheme']);
+    Route::put('softDelete/{id}', [SchemesController::class, 'softDeleteScheme']);
+    Route::put('permanentlyDelete/{id}', [SchemesController::class, 'permanentDeleteScheme']);
 
 });
